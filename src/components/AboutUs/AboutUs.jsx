86c5a22d77dad7c5ab9309d8609389e06eee1f24
Karjane98 <kajama517@gmail.com>
2023-03-07 22:27:25 +0800

    Added about us page

--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -1,13 +1,99 @@
 import { Component } from "react";
-import { Grid, Typography } from '@mui/material';
+import { Grid, Typography, Box, Divider, Avatar, Stack  } from "@mui/material";
 
 export class AboutUs extends Component {
   render() {
     return (
-      <Grid container justifyContent="space-around" alignItems="center">
-        <Typography variant="h2">
-            About Us
+      <Grid container justifyContent="space-around" alignItems="center" flexDirection="column">
+        <Typography variant="h2" sx={{ fontWeight: "bold", mt: 4 }}> About Us</Typography>
+        <Typography variant="p" sx={{ mt: 3, mx: 70 }}>
+          Get in touch with <b>FCM Trading & Services</b> today and take
+          advantage of the groundbreaking solutions we offer for home services.
+          Our optimization services provide a host of advantages, such as
+          increased productivity, cost reductions, and outstanding project
+          results. Let us help you attain your home service objectives with our
+          experienced team of professionals and inventive approach to
+          construction management.
         </Typography>
+        <Box sx={{ mb: 3 }}>
+          <img src="assets/about-us-p2.png"></img>
+        </Box>
+
+        <Typography variant="h4">Mission</Typography>
+        <Typography variant="p" sx={{ mt: 3, mb: 6, mx: 70 }}>
+          At <b>FCM Trading & Services</b>, we specialize in home services that
+          guarantee top-notch quality, safety, sustainability, and customer
+          satisfaction.
+        </Typography>
+
+        <Typography variant="h4">Vision</Typography>
+        <Typography variant="p" sx={{ mt: 3, mx: 70 }}>
+          Acknowledged as a company with remarkable skillfulness, reliability,
+          and dedication towards forming lasting, proficient, and budget
+          friendly services that fulfill the varying demands of our clients &
+          communities. We seek to accomplish this goal by means of continual
+          progress, innovation, and investing in people, systems & tech.
+        </Typography>
+        <Divider variant="middle" />
+        <Grid sx={{ display: "flex", flexDirection: "row", alignItems: "center" }}>
+          <Typography variant="h3" sx={{width: 295, height: 310, textAlign: "left", fontWeight: "bold"}}>
+            Let's start working more efficiently today!
+          </Typography>
+          <Box sx={{ mr: -35 }}>
+            <img
+              style={{ width: 632, height: 709 }}
+              src="assets/about-us-p3.png"
+            ></img>
+          </Box>
+        </Grid>
+
+        <Typography variant="h4" sx={{ fontWeight: "bold" }}>
+          Team
+        </Typography>
+        <Typography variant="subtitle1" sx={{mb: 8}}>
+          Meet the people behind our magical product
+        </Typography>
+        <Stack direction="column" spacing={10} sx={{mb: 15}}>
+          <Grid container>
+            <Grid sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', mr: 10 }}>
+              <Avatar alt="Remy Sharp" src="assets/avatar.jpg" sx={{ width: 160, height: 160, mb: 2 }} />
+              <Typography variant="h6" sx={{ fontWeight: 'bold' }}>Florentino C. Mana-ay</Typography>
+              <Typography variant="subtitle1">CEO</Typography>
+            </Grid>
+            <Grid sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', mr: 10 }}>
+              <Avatar alt="Remy Sharp" src="assets/avatar.jpg" sx={{ width: 160, height: 160, mb: 2 }} />
+              <Typography variant="h6" sx={{ fontWeight: 'bold' }}>Florentino C. Mana-ay</Typography>
+              <Typography variant="subtitle1">CEO</Typography>
+            </Grid>
+            <Grid sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', mr: 10 }}>
+              <Avatar alt="Remy Sharp" src="assets/avatar.jpg" sx={{ width: 160, height: 160, mb: 2 }} />
+              <Typography variant="h6" sx={{ fontWeight: 'bold' }}>Florentino C. Mana-ay</Typography>
+              <Typography variant="subtitle1">CEO</Typography>
+            </Grid>
+            <Grid sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', mr: 10 }}>
+              <Avatar alt="Remy Sharp" src="assets/avatar.jpg" sx={{ width: 160, height: 160, mb: 2 }} />
+              <Typography variant="h6" sx={{ fontWeight: 'bold' }}>Florentino C. Mana-ay</Typography>
+              <Typography variant="subtitle1">CEO</Typography>
+            </Grid>
+          </Grid>
+          <Grid container sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'center' }}>
+            <Grid sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', mr: 10 }}>
+              <Avatar alt="Remy Sharp" src="assets/avatar.jpg" sx={{ width: 160, height: 160, mb: 2 }} />
+              <Typography variant="h6" sx={{ fontWeight: 'bold' }}>Florentino C. Mana-ay</Typography>
+              <Typography variant="subtitle1">CEO</Typography>
+            </Grid>
+            <Grid sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', mr: 10 }}>
+              <Avatar alt="Remy Sharp" src="assets/avatar.jpg" sx={{ width: 160, height: 160, mb: 2 }} />
+              <Typography variant="h6" sx={{ fontWeight: 'bold' }}>Florentino C. Mana-ay</Typography>
+              <Typography variant="subtitle1">CEO</Typography>
+            </Grid>
+            <Grid sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', mr: 10 }}>
+              <Avatar alt="Remy Sharp" src="assets/avatar.jpg" sx={{ width: 160, height: 160, mb: 2 }} />
+              <Typography variant="h6" sx={{ fontWeight: 'bold' }}>Florentino C. Mana-ay</Typography>
+              <Typography variant="subtitle1">CEO</Typography>
+            </Grid>
+          </Grid>
+        </Stack>
       </Grid>
     );
   }
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,30 +1,94 @@
 import { Component } from "react";
 import "./Footer.scss";
-import { Box, Grid, Typography, Button } from '@mui/material';
+import { Box, Grid, Typography, Button } from "@mui/material";
+import { NavLink } from "react-router-dom";
 
 export class Footer extends Component {
   render() {
     return (
-        <Box sx={{ bgcolor: '#f5f5f5', py: 1 }}>
+      <Box sx={{ bgcolor: "#f5f5f5", py: 1 }}>
         <Grid container justifyContent="space-around" alignItems="center">
           <Grid item>
-            <Button variant="text" sx={{ '&:hover': { bgcolor: 'transparent' }, color: 'black'}} disableRipple >
-              Home
+            <Button
+              variant="text"
+              sx={{ "&:hover": { bgcolor: "transparent" }, color: "black" }}
+              disableRipple
+            >
+              <NavLink
+                to="/"
+                exact
+                activeClassName="active"
+                style={{
+                  textDecoration: "none",
+                  color: "#262626",
+                  fontWeight: "bold",
+                }}
+              >
+                Home
+              </NavLink>
             </Button>
-            <Button variant="text" sx={{ '&:hover': { bgcolor: 'transparent' }, color: 'black'}} disableRipple >
-              Service
+            <Button
+              variant="text"
+              sx={{ "&:hover": { bgcolor: "transparent" }, color: "black" }}
+              disableRipple
+            >
+              <NavLink
+                to="/services"
+                exact
+                activeClassName="active"
+                style={{
+                  textDecoration: "none",
+                  color: "#262626",
+                  fontWeight: "bold",
+                }}
+              >
+                Services
+              </NavLink>
             </Button>
-            <Button variant="text" sx={{ '&:hover': { bgcolor: 'transparent' }, color: 'black'}} disableRipple >
-              About Us
+            <Button
+              variant="text"
+              sx={{ "&:hover": { bgcolor: "transparent" }, color: "black" }}
+              disableRipple
+            >
+              <NavLink
+                to="/aboutus"
+                exact
+                activeClassName="active"
+                style={{
+                  textDecoration: "none",
+                  color: "#262626",
+                  fontWeight: "bold",
+                }}
+              >
+                About Us
+              </NavLink>
             </Button>
-            <Button variant="text" sx={{ '&:hover': { bgcolor: 'transparent' }, color: 'black'}} disableRipple >
-              Contact Us
+            <Button
+              variant="text"
+              sx={{ "&:hover": { bgcolor: "transparent" }, color: "black" }}
+              disableRipple
+            >
+              <NavLink
+                to="/contact"
+                exact
+                activeClassName="active"
+                style={{
+                  textDecoration: "none",
+                  color: "#262626",
+                  fontWeight: "bold"
+                }}
+              >
+                Contact
+              </NavLink>
             </Button>
           </Grid>
-          <Grid item sx={{ textAlign: 'right' }}>
-            <Typography variant="h5" sx={{textAlign: 'left'}}>FCM</Typography>
-            <Typography variant="body2" sx={{textAlign: 'left'}}>
-                2023 © FCM <br/>All rights reserved.
+          <Grid item sx={{ textAlign: "right" }}>
+            <Typography variant="h5" sx={{ textAlign: "left" }}>
+              FCM
+            </Typography>
+            <Typography variant="body2" sx={{ textAlign: "left" }}>
+              2023 © FCM <br />
+              All rights reserved.
             </Typography>
           </Grid>
         </Grid>
