0c6e84d1a04a5dd4302c2fd29b3c70f3b7354a5e
Karjane98 <kajama517@gmail.com>
2023-03-10 23:08:40 +0800

    Added fade effect, fix package.json

--- src/components/Homepage/Home.jsx
+++ src/components/Homepage/Home.jsx
@@ -1,195 +1,227 @@
 import { Component } from "react";
+import Fade from "react-reveal/Fade";
 import "./Home.scss";
-import { CardContent, Card, Typography, Button , Paper, Rating, Box, TextField } from '@mui/material';
-import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
-import SendIcon from '@mui/icons-material/Send';
+import {
+  CardContent,
+  Card,
+  Typography,
+  Button,
+  Paper,
+  Rating,
+  Box,
+  TextField,
+  Grid
+} from "@mui/material";
+import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
+import SendIcon from "@mui/icons-material/Send";
 
 export class Home extends Component {
   render() {
     return (
-      <body>
+      <>
         <div className="home-container">
-          <div className="home-first-content">
-            <div className="home-description">
-              <h1>Professional Help at a Reasonable Price.</h1>
-              <p>
-                FCM Trading & Services is an established construction company
-                offering reasonably priced services including roofing setup,
-                tile work, wall painting and more. Furthermore, they supply a
-                trading system for patrons to obtain construction materials at
-                competitive prices. The organization is dedicated to delivering
-                superior services and helping customers meet their building
-                ambitions within their budget limit.
-              </p>
-              <div className="header-schedule-btn">
-                <Button variant="contained" className="button-test" sx={{borderRadius: 3}}>
-                  <CalendarMonthIcon sx={{ mr: 1 }} />
-                  Consult us now
-                </Button>
-              </div>
-            </div>
-            <div>
-              <img src="assets/homepage-main.jpg" />
-            </div>
-          </div>
-          <div className="home-partners">
-            <h2>Trusted by individuals and teams in Cebu</h2>
-            <img src="assets/jollibee-logo.png" />
-            <img src="assets/mendero-logo.png" />
-          </div>
-          <div className="home-testimonials">
-            <h1>Testimonials</h1>
-            <p>People love what we do and we want to let you know</p>
-            <div className="card-testimonials" sx={{ display: "flex" }}>
-              <Card sx={{ minWidth: 275, mr: 20 }}>
-                <CardContent>
-                  <Typography
-                    variant="h6"
-                    sx={{ height: 100, width: 344, fontWeight: "bold" }}
-                    color="text.primary"
+          <Fade top distance={'80%'} duration={2500}>
+            <div className="home-first-content">
+              <div className="home-description">
+                <h1>Professional Help at a Reasonable Price.</h1>
+                <p>
+                  FCM Trading & Services is an established construction company
+                  offering reasonably priced services including roofing setup,
+                  tile work, wall painting and more. Furthermore, they supply a
+                  trading system for patrons to obtain construction materials at
+                  competitive prices. The organization is dedicated to
+                  delivering superior services and helping customers meet their
+                  building ambitions within their budget limit.
+                </p>
+                <div className="header-schedule-btn">
+                  <Button
+                    variant="contained"
+                    className="button-test"
+                    sx={{ borderRadius: 3 }}
                   >
-                    "Since implementing Lando our business has seen significant
-                    growth."
-                  </Typography>
-                  <Rating
-                    name="size-medium"
-                    sx={{ m: 3, color: "green" }}
-                    defaultValue={4}
-                    readOnly
-                  />
-                  <Typography variant="body2" sx={{ fontWeight: "bold" }}>
-                    Jack Sibre
-                  </Typography>
-                  <Typography variant="body2">Lead Manager, Growio</Typography>
-                </CardContent>
-              </Card>
-              <Card sx={{ minWidth: 275, mr: 20 }}>
-                <CardContent>
-                  <Typography
-                    variant="h6"
-                    sx={{ height: 100, width: 344, fontWeight: "bold" }}
-                    color="text.primary"
-                  >
-                    "I recommend Lando to any business looking for improvement."
-                  </Typography>
-                  <Rating
-                    name="size-medium"
-                    sx={{ m: 3, color: "green" }}
-                    defaultValue={5}
-                    readOnly
-                  />
-                  <Typography variant="body2" sx={{ fontWeight: "bold" }}>
-                    Adele Mouse
-                  </Typography>
-                  <Typography variant="body2">
-                    Product Manager, Mousio
-                  </Typography>
-                </CardContent>
-              </Card>
-              <Card sx={{ minWidth: 275 }}>
-                <CardContent>
-                  <Typography
-                    variant="h6"
-                    sx={{ height: 100, width: 344, fontWeight: "bold" }}
-                    color="text.primary"
-                  >
-                    "I can't imagine running our company without it."
-                  </Typography>
-                  <Rating
-                    name="size-medium"
-                    sx={{ m: 3, color: "green" }}
-                    defaultValue={3}
-                    readOnly
-                  />
-                  <Typography variant="body2" sx={{ fontWeight: "bold" }}>
-                    Ben Clock
-                  </Typography>
-                  <Typography variant="body2">CTO, Clockwork</Typography>
-                </CardContent>
-              </Card>
-            </div>
-          </div>
-          <div className="home-projects" sx={{ mt: 10 }}>
-            <img className="home-project-logo" src="assets/rocket.png" />
-            <h1>Our Projects</h1>
-            <div className="project-gallery">
-              <div>
-                <img src="assets/img-1.jpg" />
-                <h3>Jollibee Oriente</h3>
-              </div>
-              <div>
-                <img src="assets/img-2.jpg" />
-                <h3>Jollibee Escario</h3>
-              </div>
-              <div>
-                <img src="assets/img-3.jpg" />
-                <h3>Mendero Medical Center</h3>
-              </div>
-              <div>
-                <img src="assets/img-4.jpg" />
-                <h3>Jollibee Carcar</h3>
-              </div>
-            </div>
-          </div>
-          <div className="home-contact">
-            <Box
-              sx={{
-                display: "flex",
-                flexWrap: "wrap",
-                "& > :not(style)": {
-                  m: 1,
-                  width: 1218,
-                  height: 580,
-                  borderRadius: 5,
-                },
-              }}
-            >
-              <Paper elevation={3}>
-                <h2>Connect with us</h2>
-                <h1>Send us a message</h1>
-                <div className="contact-form">
-                  <div class="contact-name">
-                    <TextField
-                      id="outlined-basic"
-                      label="First Name"
-                      variant="outlined"
-                      sx={{ width: "50%", mr: 3 }}
-                    />
-                    <TextField
-                      id="outlined-basic"
-                      label="Last Name"
-                      variant="outlined"
-                      sx={{ width: "50%" }}
-                    />
-                  </div>
-                  <div>
-                    <TextField
-                      id="outlined-basic"
-                      label="Subject"
-                      variant="outlined"
-                      sx={{ width: "100%", mt: 2 }}
-                    />
-                  </div>
-                  <div>
-                    <TextField
-                      id="outlined-multiline-static"
-                      label="Message"
-                      placeholder="Your message here..."
-                      multiline
-                      rows={4}
-                      sx={{ width: "100%", mt: 2 }}
-                    />
-                  </div>
+                    <CalendarMonthIcon sx={{ mr: 1 }} />
+                    Consult us now
+                  </Button>
                 </div>
-                <Button variant="contained" className="button-test" sx={{borderRadius: 2, backgroundColor: 'green', }}>
-                  <SendIcon sx={{ mr: 1 }} />
-                  Send message
-                </Button>
-              </Paper>
-            </Box>
-          </div>
+              </div>
+              <div>
+                <img src="assets/homepage-main.jpg" />
+              </div>
+            </div>
+          </Fade>
+          <Fade top distance={'80%'} duration={2500}>
+            <div className="home-partners">
+              <h2>Trusted by individuals and teams in Cebu</h2>
+              <img src="assets/jollibee-logo.png" />
+              <img src="assets/mendero-logo.png" />
+            </div>
+          </Fade>
+          <Fade top duration={2500}>
+            <div className="home-testimonials">
+              <h1>Testimonials</h1>
+              <p>People love what we do and we want to let you know</p>
+              <div className="card-testimonials" sx={{ display: "flex" }}>
+                <Card sx={{ minWidth: 275, mr: 20 }}>
+                  <CardContent>
+                    <Typography
+                      variant="h6"
+                      sx={{ height: 100, width: 344, fontWeight: "bold" }}
+                      color="text.primary"
+                    >
+                      "Since implementing Lando our business has seen
+                      significant growth."
+                    </Typography>
+                    <Rating
+                      name="size-medium"
+                      sx={{ m: 3, color: "green" }}
+                      defaultValue={4}
+                      readOnly
+                    />
+                    <Typography variant="body2" sx={{ fontWeight: "bold" }}>
+                      Jack Sibre
+                    </Typography>
+                    <Typography variant="body2">
+                      Lead Manager, Growio
+                    </Typography>
+                  </CardContent>
+                </Card>
+                <Card sx={{ minWidth: 275, mr: 20 }}>
+                  <CardContent>
+                    <Typography
+                      variant="h6"
+                      sx={{ height: 100, width: 344, fontWeight: "bold" }}
+                      color="text.primary"
+                    >
+                      "I recommend Lando to any business looking for
+                      improvement."
+                    </Typography>
+                    <Rating
+                      name="size-medium"
+                      sx={{ m: 3, color: "green" }}
+                      defaultValue={5}
+                      readOnly
+                    />
+                    <Typography variant="body2" sx={{ fontWeight: "bold" }}>
+                      Adele Mouse
+                    </Typography>
+                    <Typography variant="body2">
+                      Product Manager, Mousio
+                    </Typography>
+                  </CardContent>
+                </Card>
+                <Card sx={{ minWidth: 275 }}>
+                  <CardContent>
+                    <Typography
+                      variant="h6"
+                      sx={{ height: 100, width: 344, fontWeight: "bold" }}
+                      color="text.primary"
+                    >
+                      "I can't imagine running our company without it."
+                    </Typography>
+                    <Rating
+                      name="size-medium"
+                      sx={{ m: 3, color: "green" }}
+                      defaultValue={3}
+                      readOnly
+                    />
+                    <Typography variant="body2" sx={{ fontWeight: "bold" }}>
+                      Ben Clock
+                    </Typography>
+                    <Typography variant="body2">CTO, Clockwork</Typography>
+                  </CardContent>
+                </Card>
+              </div>
+            </div>
+          </Fade>
+          <Fade top distance={'80%'} duration={2500}>
+            <div className="home-projects" sx={{ mt: 10 }}>
+              <img className="home-project-logo" src="assets/rocket.png" />
+              <h1>Our Projects</h1>
+              <div className="project-gallery">
+                <Grid>
+                  <Box component="img"sx={{height: 312, borderRadius: 16, mb: 3}} src="assets/img-1.jpg"></Box>
+                  <Typography variant="h5">Jollibee Oriente</Typography>
+                </Grid>
+                <Grid>
+                  <Box component="img"sx={{height: 312, borderRadius: 16, mb: 3}} src="assets/img-2.jpg"></Box>
+                  <Typography variant="h5">Jollibee Carcar</Typography>
+                </Grid>
+                <Grid>
+                  <Box component="img"sx={{height: 312, borderRadius: 16, mb: 3}} src="assets/img-3.jpg"></Box>
+                  <Typography variant="h5">Jollibee Jones</Typography>
+                </Grid>
+                <Grid>
+                  <Box component="img"sx={{height: 312, borderRadius: 16, mb: 3}} src="assets/img-4.jpg"></Box>
+                  <Typography variant="h5">Mendero Medical Center</Typography>
+                </Grid>
+              </div>
+            </div>
+          </Fade>
+          <Fade top distance={'80%'} duration={2500}>
+            <div className="home-contact">
+              <Box
+                sx={{
+                  display: "flex",
+                  flexWrap: "wrap",
+                  "& > :not(style)": {
+                    m: 1,
+                    width: 1218,
+                    height: 580,
+                    borderRadius: 5,
+                  },
+                }}
+              >
+                <Paper elevation={3}>
+                  <h2>Connect with us</h2>
+                  <h1>Send us a message</h1>
+                  <div className="contact-form">
+                    <div class="contact-name">
+                      <TextField
+                        id="outlined-basic"
+                        label="First Name"
+                        variant="outlined"
+                        sx={{ width: "50%", mr: 3 }}
+                      />
+                      <TextField
+                        id="outlined-basic"
+                        label="Last Name"
+                        variant="outlined"
+                        sx={{ width: "50%" }}
+                      />
+                    </div>
+                    <div>
+                      <TextField
+                        id="outlined-basic"
+                        label="Subject"
+                        variant="outlined"
+                        sx={{ width: "100%", mt: 2 }}
+                      />
+                    </div>
+                    <div>
+                      <TextField
+                        id="outlined-multiline-static"
+                        label="Message"
+                        placeholder="Your message here..."
+                        multiline
+                        rows={4}
+                        sx={{ width: "100%", mt: 2 }}
+                      />
+                    </div>
+                  </div>
+                  <Button
+                    variant="contained"
+                    className="button-test"
+                    sx={{ borderRadius: 2, backgroundColor: "green" }}
+                  >
+                    <SendIcon sx={{ mr: 1 }} />
+                    Send message
+                  </Button>
+                </Paper>
+              </Box>
+            </div>
+          </Fade>
         </div>
-      </body>
+      </>
     );
   }
 }
--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -6,62 +6,71 @@ import IronIcon from '@mui/icons-material/Iron';
 import HouseIcon from '@mui/icons-material/House';
 import ConstructionIcon from '@mui/icons-material/Construction';
 import ElectricalServicesIcon from '@mui/icons-material/ElectricalServices';
+import Fade from "react-reveal/Fade";
 
 
 export class Services extends Component {
   render() {
     return (
       <Grid container justifyContent="space-around" alignItems="center">
-        <Typography variant="h2">Services</Typography>
-        <Typography variant="subtitle1" sx={{ mt: 3, mx: 70, mb: 5}}>FCM Trading & Services offers a broad selection of construction services, such as roof installation, tile work and wall painting. Furthermore, they have a trading platform that allows clients to source construction materials at competitive rates. With their pledge to deliver top-notch services and cost-efficient solutions, FCM Trading & Services helps customers reach their building objectives in an effective and efficient way.</Typography>
+        <Fade top distance={'80%'} duration={2000}>
+          <Typography variant="h2">Services</Typography>
+          <Typography variant="subtitle1" sx={{ mt: 3, mx: 70, mb: 5}}>FCM Trading & Services offers a broad selection of construction services, such as roof installation, tile work and wall painting. Furthermore, they have a trading platform that allows clients to source construction materials at competitive rates. With their pledge to deliver top-notch services and cost-efficient solutions, FCM Trading & Services helps customers reach their building objectives in an effective and efficient way.</Typography>
+        </Fade>
 
         <Stack direction='row' sx={{flexWrap: 'wrap', justifyContent: 'center'}}>
-          <Grid sx={{mr: 10}}>
-            <Paper elevation={3} sx={{mb: 4}}>
-              <Grid container direction='column' sx={{height: 600, width: 600, p: 2}}>
-                <Box sx={{display: 'flex', flexDirection:'row', alignItems: 'center', mb: 2}}>
-                  <ImagesearchRollerIcon sx={{mr: 0.5}}/>
-                  <Typography variant="h5" sx={{fontWeight: 'bold'}}>Construction</Typography>
-                </Box>
-                <Box component="img"sx={{height: 300, mb: 3}} src="assets/construction.jpeg"></Box>
-                <Typography variant="subtitle1">We offer a wide range of construction services, including <b>high-quality building materials such as cement, CHB, sand, gravel, and various lumber and wood options.</b> Our team ensures that all materials are sourced from reputable suppliers and treated with utmost care during installation. With a focus on quality, safety, and efficiency, we are committed to delivering exceptional results for your building project.</Typography>
-              </Grid>
-            </Paper>
-            <Paper elevation={3}>
-              <Grid container direction='column' sx={{height: 600, width: 600, p: 2}}>
-                <Box sx={{display: 'flex', flexDirection:'row', alignItems: 'center', mb: 2}}>
-                  <GridViewIcon sx={{mr: 0.5}}/>
-                  <Typography variant="h5" sx={{fontWeight: 'bold'}}>Hardware</Typography>
-                </Box>
-                <Box component="img"sx={{height: 300, mb: 3}} src="assets/hardware.jpeg"></Box>
-                <Typography variant="subtitle1">We provide a comprehensive range of hardware products, <b>including all types of tools, equipment, and power tools from trusted brands like BOSCH and MAKITA.</b> We also offer high-quality stainless sheet and bars for a variety of applications. Our goal is to provide our customers with the best hardware solutions for their needs, ensuring both quality and affordability.</Typography>
-              </Grid>
-            </Paper>
-          </Grid>
+          <Fade top distance={'80%'} duration={2000}>
+            <Grid sx={{mr: 10}}>
+              <Paper elevation={3} sx={{mb: 4}}>
+                <Grid container direction='column' sx={{height: 600, width: 600, p: 2}}>
+                  <Box sx={{display: 'flex', flexDirection:'row', alignItems: 'center', mb: 2}}>
+                    <ImagesearchRollerIcon sx={{mr: 0.5}}/>
+                    <Typography variant="h5" sx={{fontWeight: 'bold'}}>Construction</Typography>
+                  </Box>
+                  <Box component="img"sx={{height: 300, mb: 3}} src="assets/construction.jpeg"></Box>
+                  <Typography variant="subtitle1">We offer a wide range of construction services, including <b>high-quality building materials such as cement, CHB, sand, gravel, and various lumber and wood options.</b> Our team ensures that all materials are sourced from reputable suppliers and treated with utmost care during installation. With a focus on quality, safety, and efficiency, we are committed to delivering exceptional results for your building project.</Typography>
+                </Grid>
+              </Paper>
+              <Paper elevation={3}>
+                <Grid container direction='column' sx={{height: 600, width: 600, p: 2}}>
+                  <Box sx={{display: 'flex', flexDirection:'row', alignItems: 'center', mb: 2}}>
+                    <GridViewIcon sx={{mr: 0.5}}/>
+                    <Typography variant="h5" sx={{fontWeight: 'bold'}}>Hardware</Typography>
+                  </Box>
+                  <Box component="img"sx={{height: 300, mb: 3}} src="assets/hardware.jpeg"></Box>
+                  <Typography variant="subtitle1">We provide a comprehensive range of hardware products, <b>including all types of tools, equipment, and power tools from trusted brands like BOSCH and MAKITA.</b> We also offer high-quality stainless sheet and bars for a variety of applications. Our goal is to provide our customers with the best hardware solutions for their needs, ensuring both quality and affordability.</Typography>
+                </Grid>
+              </Paper>
+            </Grid>
+          </Fade>
 
-          <Grid sx={{mb: 4}}>
-            <Paper elevation={3} sx={{mb: 4}}>
-              <Grid container direction='column' sx={{height: 600, width: 600, p: 2}}>
-                <Box sx={{display: 'flex', flexDirection:'row', alignItems: 'center', mb: 2}}>
-                  <IronIcon sx={{mr: 0.5}}/>
-                  <Typography variant="h5" sx={{fontWeight: 'bold'}}>Iron Works</Typography>
-                </Box>
-                <Box component="img"sx={{height: 300, mb: 3}} src="assets/iron-works.jpeg"></Box>
-                <Typography variant="subtitle1">Our iron works services include the installation of steel gates, window grills, and roll-up doors. Our team is highly skilled in the fabrication and installation of these products, ensuring that they are both sturdy and visually appealing. Contact us today to learn more about our iron works services and how we can help you achieve your construction goals.</Typography>
-              </Grid>
-            </Paper>
-            <Paper elevation={3}>
-              <Grid container direction='column' sx={{height: 600, width: 600, p: 2}}>
-                <Box sx={{display: 'flex', flexDirection:'row', alignItems: 'center', mb: 2}}>
-                  <HouseIcon sx={{mr: 0.5}}/>
-                  <Typography variant="h5" sx={{fontWeight: 'bold'}}>Glassworks / Aluminum</Typography>
-                </Box>
-                <Box component="img"sx={{height: 300, mb: 3}} src="assets/door-glass-installation.jpeg"></Box>
-                <Typography variant="subtitle1">We offer expert <b>glassworks and aluminum construction services, including installation of sliding and swing doors, windows, screens, and jalousie products.</b> Our team is dedicated to providing high-quality solutions that are both durable and visually appealing. Contact us today to learn more about how we can bring your construction project to life.</Typography>
-              </Grid>
-            </Paper>
-          </Grid>
+          <Fade top distance={'80%'} duration={2000}>
+            <Grid sx={{mb: 4}}>
+              <Paper elevation={3} sx={{mb: 4}}>
+                <Grid container direction='column' sx={{height: 600, width: 600, p: 2}}>
+                  <Box sx={{display: 'flex', flexDirection:'row', alignItems: 'center', mb: 2}}>
+                    <IronIcon sx={{mr: 0.5}}/>
+                    <Typography variant="h5" sx={{fontWeight: 'bold'}}>Iron Works</Typography>
+                  </Box>
+                  <Box component="img"sx={{height: 300, mb: 3}} src="assets/iron-works.jpeg"></Box>
+                  <Typography variant="subtitle1">Our iron works services include the installation of steel gates, window grills, and roll-up doors. Our team is highly skilled in the fabrication and installation of these products, ensuring that they are both sturdy and visually appealing. Contact us today to learn more about our iron works services and how we can help you achieve your construction goals.</Typography>
+                </Grid>
+              </Paper>
+              <Paper elevation={3}>
+                <Grid container direction='column' sx={{height: 600, width: 600, p: 2}}>
+                  <Box sx={{display: 'flex', flexDirection:'row', alignItems: 'center', mb: 2}}>
+                    <HouseIcon sx={{mr: 0.5}}/>
+                    <Typography variant="h5" sx={{fontWeight: 'bold'}}>Glassworks / Aluminum</Typography>
+                  </Box>
+                  <Box component="img"sx={{height: 300, mb: 3}} src="assets/door-glass-installation.jpeg"></Box>
+                  <Typography variant="subtitle1">We offer expert <b>glassworks and aluminum construction services, including installation of sliding and swing doors, windows, screens, and jalousie products.</b> Our team is dedicated to providing high-quality solutions that are both durable and visually appealing. Contact us today to learn more about how we can bring your construction project to life.</Typography>
+                </Grid>
+              </Paper>
+            </Grid>
+          </Fade>
 
+
+          <Fade top distance={'80%'} duration={2000}>
           <Grid sx={{display: 'flex'}}>
             <Paper elevation={3} sx={{mb: 4, mr: 10}}>
               <Grid container direction='column' sx={{height: 600, width: 600, p: 2}}>
@@ -84,6 +93,7 @@ export class Services extends Component {
               </Grid>
             </Paper>
           </Grid>
+          </Fade>
         </Stack>
       </Grid>
     );
