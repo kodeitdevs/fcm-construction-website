3499db284829ab742cc8ee1a0d3362cb7258f9bc
Karjane98 <kajama517@gmail.com>
2023-03-14 22:35:23 +0800

    Added schedule stepper

--- src/components/Schedule/Schedule.jsx
+++ src/components/Schedule/Schedule.jsx
@@ -1,12 +1,33 @@
-import { Paper, Grid, Button, Typography } from "@mui/material";
+import { Paper, Grid, Box, Typography, Stepper, Step, StepLabel, Button } from "@mui/material";
 import React, { useState } from "react";
 import CloseIcon from '@mui/icons-material/Close';
 
 function Schedule() {
-  const [setOpenModal] = useState(false);
+  const [open, setOpenModal] = useState(false);
+  const [activeStep, setActiveStep] = React.useState(0);
+
   const handleCloseModal = () => {
     setOpenModal(false);
   };
+
+  const steps = [
+    'Select a Date & Time',
+    'Fill up Information',
+    'Verify',
+    'Done'
+  ];
+
+  const nextStep = () => {
+    if(activeStep < 3) {
+        setActiveStep((currentStep) => currentStep + 1)
+    }
+  }
+
+  const prevStep = () => {
+    if(activeStep !== 0) {
+        setActiveStep((currentStep) => currentStep - 1)
+    }
+  }
 
   return (
     <Grid container justifyContent="center">
@@ -14,7 +35,21 @@ function Schedule() {
         <Grid container justifyContent="flex-end" sx={{p: 2}}>
             <CloseIcon onClick={handleCloseModal}/>
         </Grid>
-        <Typography variant="h3" sx={{display: 'flex', justifyContent: 'center'}}>Schedule a Consultation</Typography>
+        <Typography variant="h3" sx={{display: 'flex', justifyContent: 'center', mb: 5}}>Schedule a Consultation</Typography>
+        <Box sx={{width: '100%'}}>
+            <Stepper activeStep={activeStep} alternativeLabel>
+                {steps.map((label) => (
+                    <Step key={label}>
+                        <StepLabel>{label}</StepLabel>
+                    </Step>
+                ))}
+            </Stepper>
+        </Box>
+        <h1>{activeStep}</h1>
+        <Grid container justifyContent='end'>
+            <Button onClick={() => prevStep()}>Back</Button>          
+            <Button onClick={() => nextStep()}>Next</Button>
+        </Grid>
       </Paper>
     </Grid>
   );
